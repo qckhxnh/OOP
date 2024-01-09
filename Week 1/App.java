4050cc15ed05a1d3333064f784224226b5ebe817
qckhxnh <khanh.kieu@student.lut.fi>
2024-01-09 20:17:05 +0200

    git commit

--- Week 1/App.java
+++ Week 1/App.java
@@ -1,5 +1,5 @@
-public class test {
-    public static void App(String[] args) {
+public class App {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
