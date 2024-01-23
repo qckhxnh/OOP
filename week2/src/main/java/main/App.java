b04f8943f1c8ca99b3de73accb78f86203048198
qckhxnh <khanh.kieu@student.lut.fi>
2024-01-23 16:17:03 +0200

    git commit -m "final commit"

--- week2/src/main/java/main/App.java
+++ week2/src/main/java/main/App.java
@@ -1,52 +1,50 @@
 package main;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) {
-        Hedgehog hedgehog = new Hedgehog();
         Scanner scanner = new Scanner(System.in);
-        int choice;
-        System.out.println("1) Make hedgehog talk, 2) Create new hedgehog, 3) Make hedgehog run, 0) Quit");
-        choice = scanner.nextInt();
-        while (true) {
-            if (choice == 1) {
-                System.out.println("What does the hedgehog say?");
-                String message = scanner.nextLine(); 
-                hedgehog.speak(message);
+        Hedgehog hedgehog = new Hedgehog();
+
+        do {
+            while (true){
                 System.out.println("1) Make hedgehog talk, 2) Create new hedgehog, 3) Make hedgehog run, 0) Quit");
-                choice = scanner.nextInt();
-            }
-            else if (choice == 2) {
-                System.out.println("What is the name of the hedgehog?");
-                String name = scanner.next();
-                System.out.println("What is the age of the hedgehog?");
-                try {
-                    int age = scanner.nextInt();
-                    hedgehog = new Hedgehog(name, age);
-                    System.out.println("1) Make hedgehog talk, 2) Create new hedgehog, 3) Make hedgehog run, 0) Quit");
-                    choice = scanner.nextInt();
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input for age. Please enter a valid integer.");
-                    scanner.nextLine(); 
-                }
-            }
-            else if (choice == 3) {
-                System.out.println("How many laps does the hedgehog run?");
-                int laps = scanner.nextInt();
-                hedgehog.run(laps);
-                System.out.println("1) Make hedgehog talk, 2) Create new hedgehog, 3) Make hedgehog run, 0) Quit");
-                choice = scanner.nextInt();
-            }
-            else if (choice == 0) {
-                System.out.println("Thank you for using the program.");
-                break;
-            }
-            else {
-                System.out.println("Wrong input value");
-                System.out.println("1) Make hedgehog talk, 2) Create new hedgehog, 3) Make hedgehog run, 0) Quit");
-                choice = scanner.nextInt();
-            }
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("What does hedgehog say?");
+                        String words = scanner.nextLine();
+                        hedgehog.speak(words);
+                        break;
+                    
+                    case 2:
+                        System.out.println("What is the name of the hedgehog:");
+                        String name = scanner.nextLine();
+                        System.out.println("What is the age of the hedgehog:");
+                        int age = scanner.nextInt();
+                        scanner.nextLine();
+                        hedgehog = new Hedgehog(name, age);
+                        break;
+                    
+                    case 3:
+                        System.out.println("How many laps?");
+                        int laps = scanner.nextInt();
+                        scanner.nextLine();
+                        hedgehog.run(laps);
+                        break;
+                    
+                    case 0:
+                        System.out.println("Thank you for using the program.");
+                        scanner.close();
+                        return;
+
+                    default:
+                        System.out.println("Wrong input value");
+                        break;
+        }
     }
-    }
+        } while (scanner.hasNextLine());
 }
+};
--- week2/src/main/java/main/Hedgehog.java
+++ week2/src/main/java/main/Hedgehog.java
@@ -1,35 +1,29 @@
 package main;
 
 class Hedgehog {
-    private String name;
-    private int age;
-    
-    //Default constructor
-    public Hedgehog() {
+    String name;
+    int age;
+
+    Hedgehog() {
         this.name = "Pikseli";
         this.age = 5;
     }
-    
-    // Parameterized constructor
-    public Hedgehog(String name, int age) {
+    Hedgehog(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Method: Speak
-    public void speak(String message) {
-        if (message.length() == 0) {
-            System.out.println("I am " + name + " and my age is " + age + ", but could you still give me input values?");;
-        }
-        else {
-            System.out.println(name + ": " + message);
+    void speak(String words) {
+        if (words.isEmpty()) {
+            System.out.println("I am " + this.name + " and my age is " + this.age + ", but could you still give me input values?");
+        } else {
+            System.out.println(this.name + ": " + words);
         }
     }
 
-    // Method: Run
-    public void run(int laps){
+    void run(int laps) {
         for (int i = 0; i < laps; i++) {
-            System.out.println(name + " runs really fast!");
+            System.out.println(this.name + " runs really fast!");
         }
     }
 }
